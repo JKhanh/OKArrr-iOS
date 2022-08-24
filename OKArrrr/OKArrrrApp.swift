f791ccf1d2f5f9a7763bf0de25421d3593d0f258
Harry Nguyen <nguyenkhanhcv@gmail.com>
2022-08-24 21:37:35 +0700

    Initial Commit

---
+++ OKArrrr/OKArrrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKArrrrApp.swift
+//  OKArrrr
+//
+//  Created by Khanh jonas on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct OKArrrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
